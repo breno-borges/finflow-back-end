a019f291df3bdfdf116a9a086622de3661d6f597
Breno Borges <breno.borges.15@outlook.com>
2024-10-26 14:54:22 -0300

    cors

---
+++ src/main/java/br/com/brenoborges/finflow/security/CorsConfig.java
@@ -0,0 +1,22 @@
+package br.com.brenoborges.finflow.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS, PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
